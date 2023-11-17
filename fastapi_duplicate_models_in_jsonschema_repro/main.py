011b89b0f3637da4e942c7c99cf68aa876a6f268
Hendrik Lücke-Tieke <hendrik.luecke-tieke@igd.fraunhofer.de>
2023-11-17 10:31:08 +0100

    tests

--- fastapi_duplicate_models_in_jsonschema_repro/main.py
+++ fastapi_duplicate_models_in_jsonschema_repro/main.py
@@ -8,6 +8,7 @@ from uuid import UUID, uuid4
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
+
 class PetType(str, Enum):
     CAT = "cat"
     DOG = "dog"
@@ -18,25 +19,37 @@ class Pet(BaseModel):
     name: str = "Unnamed pet"
     petType: str
 
+
 class HuntingSkill(Enum):
     CLUELESS = "clueless"
     LAZY = "lazy"
     ADVENTUROUS = "adventurous"
     AGGRESSIVE = "aggressive"
 
+
 class Cat(Pet):
-    petType: Literal[PetType.CAT] = PetType.CAT
-    huntingSkill: Annotated[HuntingSkill, Field(default=HuntingSkill.LAZY, description="The measured skill for hunting")]
+    petType: Literal[PetType.CAT] = PetType.CAT  # missing in constructor
+    # petType = PetType.CAT # illegal
+    # petType = Literal[PetType.CAT]  # https://errors.pydantic.dev/2.5/u/model-field-overridden
+    # petType = Annotated[Literal[PetType.CAT], Field(default=PetType.CAT)]  # https://errors.pydantic.dev/2.5/u/model-field-overridden
+    # petType = Literal[PetType.CAT] = Field(default=PetType.CAT) # TypeError: '_LiteralSpecialForm' object does not support item assignment
+    huntingSkill: Annotated[
+        HuntingSkill,
+        Field(default=HuntingSkill.LAZY, description="The measured skill for hunting"),
+    ]
+
 
 class Dog(Pet):
     petType: Literal[PetType.DOG] = PetType.DOG
-    packSize: Annotated[int, Field(json_schema_extra={
-        "format":"int32",
-        "minimum": 0,
-        "default": 0
-    })]
+    # petType = PetType.DOG # illegal
+    packSize: Annotated[
+        int, Field(json_schema_extra={
+            "format": "int32",
+            "minimum": 0,
+            "default": 0
+        })
+    ]
 
-Cohort = Annotated[Union[Dog, Cat], Field(..., discriminator="petType")]
 
 # register startup event
 @asynccontextmanager
@@ -44,28 +57,39 @@ async def lifespan(application: FastAPI):
     dump_openapi(application)
     yield
 
+
 # Init FastAPI and connect routers
 app = FastAPI(debug=True, lifespan=lifespan)
 
-@app.get("/") 
+
+@app.get("/")
 async def list() -> List[Pet]:
-    return [Cat(name="Felix"), Cat(name="Kitty"), Dog(name="Wolf"), Dog(name="Rex")]
+    return [
+        Cat(name="Felix"),
+        Cat(name="Kitty"),
+        Dog(name="Wolf", packSize=24),
+        Dog(name="Rex", packSize=2)
+    ]
+
 
 @app.post("/")
 async def update(foo: Pet) -> Pet:
     foo.id = uuid4()
 
+
 @app.get("/get_cat")
 async def get_cat(q: str) -> Cat:
     return Cat(name=q)
+
 
 @app.get("/get_dog")
 async def get_dog(q: str) -> Dog:
     return Dog(name=q)
 
+
 def dump_openapi(application: FastAPI):
     target = Path("build/openapi.json")
     os.makedirs("build", exist_ok=True)
     if application.openapi() is not None:
-        with open(target, 'w') as f:
+        with open(target, "w") as f:
             json.dump(application.openapi(), f, sort_keys=True, indent=True)
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,21 +5,20 @@ from fastapi_duplicate_models_in_jsonschema_repro.main import Cat, Pet, PetType,
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/get_cat?q=flix")
-    assert response.status_code == 200
-    r = response.json()
-    cat = Cat(**r)
-    assert cat.name == "flix"
+# def test_read_main():
+#     response = client.get("/get_cat?q=flix")
+#     assert response.status_code == 200
+#     r = response.json()
+#     cat = Cat(**r)
+#     assert cat.name == "flix"
 
-def list_pets():
+
+def test_list_pets():
     response = client.get("/")
     assert response.status_code == 200
     r = response.json()
-    parsed = [Pet(x) for x in r]
+    parsed = [Pet(**x) for x in r]
 
-    # assert len(parsed) == 4
-    # assert parsed[0].petType == PetType.CAT
-    # assert False
-
+    assert len(parsed) == 4
+    assert parsed[0].petType == PetType.CAT
     
